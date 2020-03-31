fdb91a940469643ccb56c27a1145793dc3b46425
Wenhao Guo <gwh1742072653@gmail.com>
2020-03-31 19:23:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence1)
   array = sentence1.split(" ")
-  array1 = []
-  array.each do |word|
-    array1 << word.reverse
-  end
+  array1 = array.collect do |word|
+    word.reverse
+ end
  array1.join(" ")
 end
